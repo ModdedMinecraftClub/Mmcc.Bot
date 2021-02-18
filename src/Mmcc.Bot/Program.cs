ff293cd93a0b77abd5e4a9e4867390e805da410f
Trace <lukaszdabrowski881@gmail.com>
2021-02-18 06:39:42 +0000

    update caching settings

--- src/Mmcc.Bot/Program.cs
+++ src/Mmcc.Bot/Program.cs
@@ -98,20 +98,20 @@ namespace Mmcc.Bot
                     services.AddDiscordCaching();
                     services.Configure<CacheSettings>(settings =>
                     {
-                        settings.SetAbsoluteExpiration<IGuild>(TimeSpan.FromDays(1));
+                        settings.SetAbsoluteExpiration<IGuild>(TimeSpan.FromDays(2));
                         settings.SetSlidingExpiration<IGuild>(TimeSpan.FromDays(1));
                         
-                        settings.SetAbsoluteExpiration<IRole>(TimeSpan.FromDays(1));
+                        settings.SetAbsoluteExpiration<IRole>(TimeSpan.FromDays(2));
                         settings.SetSlidingExpiration<IRole>(TimeSpan.FromDays(1));
                         
-                        settings.SetAbsoluteExpiration<IReadOnlyList<IRole>>(TimeSpan.FromDays(1));
+                        settings.SetAbsoluteExpiration<IReadOnlyList<IRole>>(TimeSpan.FromDays(2));
                         settings.SetSlidingExpiration<IReadOnlyList<IRole>>(TimeSpan.FromDays(1));
 
                         settings.SetAbsoluteExpiration<IChannel>(TimeSpan.FromDays(1));
-                        settings.SetSlidingExpiration<IChannel>(TimeSpan.FromHours(12));
+                        settings.SetSlidingExpiration<IChannel>(TimeSpan.FromHours(6));
 
-                        settings.SetAbsoluteExpiration<IGuildMember>(TimeSpan.FromHours(30));
-                        settings.SetSlidingExpiration<IGuildMember>(TimeSpan.FromMinutes(15));
+                        settings.SetAbsoluteExpiration<IGuildMember>(TimeSpan.FromHours(12));
+                        settings.SetSlidingExpiration<IGuildMember>(TimeSpan.FromHours(3));
                     });
                 });
     }
