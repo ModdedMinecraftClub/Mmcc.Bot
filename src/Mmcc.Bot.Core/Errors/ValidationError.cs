ab949c8a098f9e3ac4fcb8ad4ac124cf5b834032
Trace <lukaszdabrowski881@gmail.com>
2021-02-19 20:59:24 +0000

    remove redundant arg

--- src/Mmcc.Bot.Core/Errors/ValidationError.cs
+++ src/Mmcc.Bot.Core/Errors/ValidationError.cs
@@ -6,5 +6,5 @@ namespace Mmcc.Bot.Core.Errors
     /// <summary>
     /// Represents a parameter validation error.
     /// </summary>
-    public record ValidationError(string Resource, string Message, IChildNode? Node = default) : ResultError(Message);
+    public record ValidationError(string Message, IChildNode? Node = default) : ResultError(Message);
 }
