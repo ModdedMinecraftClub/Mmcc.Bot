ad018026020a73f1f8fb3fbd172226f06b2c2262
Trace <lukaszdabrowski881@gmail.com>
2021-02-25 19:59:01 +0000

    format comments

--- src/Mmcc.Bot.Infrastructure/Parsers/TimeSpanParser.cs
+++ src/Mmcc.Bot.Infrastructure/Parsers/TimeSpanParser.cs
@@ -15,21 +15,21 @@ namespace Mmcc.Bot.Infrastructure.Parsers
     {
         private static readonly string[] Formats =
         {
-            "%d'd'%h'h'%m'm'%s's'", //4d3h2m1s
-            "%d'd'%h'h'%m'm'", //4d3h2m
-            "%d'd'%h'h'%s's'", //4d3h1s
-            "%d'd'%h'h'", //4d3h
-            "%d'd'%m'm'%s's'", //4d2m1s
-            "%d'd'%m'm'", //4d2m
-            "%d'd'%s's'", //4d1s
-            "%d'd'", //4d
-            "%h'h'%m'm'%s's'", //3h2m1s
-            "%h'h'%m'm'", //3h2m
-            "%h'h'%s's'", //3h1s
-            "%h'h'", //3h
-            "%m'm'%s's'", //2m1s
-            "%m'm'", //2m
-            "%s's'", //1s
+            "%d'd'%h'h'%m'm'%s's'", // 4d3h2m1s
+            "%d'd'%h'h'%m'm'",      // 4d3h2m
+            "%d'd'%h'h'%s's'",      // 4d3h1s
+            "%d'd'%h'h'",           // 4d3h
+            "%d'd'%m'm'%s's'",      // 4d2m1s
+            "%d'd'%m'm'",           // 4d2m
+            "%d'd'%s's'",           // 4d1s
+            "%d'd'",                // 4d
+            "%h'h'%m'm'%s's'",      // 3h2m1s
+            "%h'h'%m'm'",           // 3h2m
+            "%h'h'%s's'",           // 3h1s
+            "%h'h'",                // 3h
+            "%m'm'%s's'",           // 2m1s
+            "%m'm'",                // 2m
+            "%s's'",                // 1s
         };
         
         /// <inheritdoc />
