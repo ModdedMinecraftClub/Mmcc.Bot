47c558c43972f9b00bfee260bfd7fd613c37e77f
Trace <lukaszdabrowski881@gmail.com>
2021-02-26 23:08:02 +0000

    add tools

---
+++ src/Mmcc.Bot.Tools.DbConverter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mmcc.Bot.Tools.DbConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/Mmcc.Bot.Tools.DbConverter/README.md
@@ -0,0 +1,7 @@
+﻿# Mmcc.Bot.Tools.DbConverter
+
+CLI tool to aid database migration from old member bot (Mmcc.MemberBot) to the new bot (Mmcc.Bot).
+
+## Usage
+
+TODO
