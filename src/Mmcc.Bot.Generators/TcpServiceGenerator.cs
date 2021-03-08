27119c8853e865bf8a22c8f60addb9e03c7bb37a
Trace <lukaszdabrowski881@gmail.com>
2021-03-08 11:59:42 +0000

    remove unused usings

--- src/Mmcc.Bot.Generators/TcpServiceGenerator.cs
+++ src/Mmcc.Bot.Generators/TcpServiceGenerator.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Linq;
 using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Text;
