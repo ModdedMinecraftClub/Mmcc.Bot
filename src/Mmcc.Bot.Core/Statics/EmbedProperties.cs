411b0487bc6f748ea1677443dce972988fada040
Trace <lukaszdabrowski881@gmail.com>
2021-03-09 15:52:48 +0000

    code cleanup

--- src/Mmcc.Bot.Core/Statics/EmbedProperties.cs
+++ src/Mmcc.Bot.Core/Statics/EmbedProperties.cs
@@ -1,11 +1,9 @@
 ﻿using Remora.Discord.API.Objects;
-using Remora.Discord.Core;
 
 namespace Mmcc.Bot.Core.Statics
 {
     public class EmbedProperties
     {
-        public static EmbedThumbnail MmccLogoThumbnail => new(Urls.MmccLogoUrl, new Optional<string>(),
-            new Optional<int>(), new Optional<int>());
+        public static EmbedThumbnail MmccLogoThumbnail => new(Urls.MmccLogoUrl);
     }
 }
