372404604d2c5966dc96331f3c1e957c387f83c0
Trace <lukaszdabrowski881@gmail.com>
2021-03-09 08:12:25 +0000

    add comments

--- src/Mmcc.Bot.Infrastructure/Requests/Generic/TcpRequest.cs
+++ src/Mmcc.Bot.Infrastructure/Requests/Generic/TcpRequest.cs
@@ -4,11 +4,27 @@ using Ssmp;
 
 namespace Mmcc.Bot.Infrastructure.Requests.Generic
 {
+    /// <summary>
+    /// Represents a request coming from a TCP client.
+    /// </summary>
+    /// <typeparam name="T">Request message type.</typeparam>
     public class TcpRequest<T> : IRequest where T : IMessage<T>
     {
+        /// <summary>
+        /// Request's author.
+        /// </summary>
         public ConnectedClient ConnectedClient { get; }
+        
+        /// <summary>
+        /// Request's message (body).
+        /// </summary>
         public T? Message { get; }
 
+        /// <summary>
+        /// Instantiates a new instance of <see cref="TcpRequest{T}"/>.
+        /// </summary>
+        /// <param name="connectedClient">Request's author.</param>
+        /// <param name="message">Request's message (body).</param>
         public TcpRequest(ConnectedClient connectedClient, T? message)
         {
             ConnectedClient = connectedClient;
