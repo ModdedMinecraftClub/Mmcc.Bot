9b7f08f37f2ed3778ed7ad88725aaf02edec6a80
Trace <lukaszdabrowski881@gmail.com>
2021-03-22 03:03:12 +0000

    separate into a mediator query

--- src/Mmcc.Bot/CommandGroups/Core/CoreGuildCommands.cs
+++ src/Mmcc.Bot/CommandGroups/Core/CoreGuildCommands.cs
@@ -57,54 +57,37 @@ namespace Mmcc.Bot.CommandGroups.Core
         [Command("guild")]
         [Description("Provides information about the current guild.")]
         [RequireGuild]
-        // TODO: separate into a mediator query;
         public async Task<IResult> GuildInfo()
         {
-            var getGuildInfoResult = await _guildApi.GetGuildAsync(_context.GuildID.Value);
-            if (!getGuildInfoResult.IsSuccess)
+            var queryResult = await _mediator.Send(new GetGuildInfo.Query(_context.GuildID.Value));
+
+            if (!queryResult.IsSuccess)
             {
-                return getGuildInfoResult;
+                return queryResult;
             }
 
-            var guildInfo = getGuildInfoResult.Entity;
-            if (guildInfo is null)
-            {
-                return Result.FromError(new NotFoundError("Guild not found."));
-            }
-
+            var guildInfo = queryResult.Entity;
             var embed = new Embed
             {
                 Title = "Guild info",
                 Description = "Information about the current guild.",
                 Fields = new List<EmbedField>
                 {
-                    new("Name", guildInfo.Name, false),
-                    new("Owner", $"<@{guildInfo.OwnerID}>"),
-                    new("Max members",
-                        guildInfo.MaxMembers.HasValue
-                            ? guildInfo.MaxMembers.Value.ToString()
-                            : "Unavailable", false),
-                    new("Available roles",
-                        string.Join(", ",
-                            guildInfo.Roles
-                                .Skip(1)
-                                .Select(r => $"<@&{r.ID}>")))
+                    new("Name", guildInfo.GuildName, false),
+                    new("Owner", $"<@{guildInfo.GuildOwnerId}>"),
+                    new("Max members", guildInfo.GuildMaxMembers.ToString() ?? "Unavailable", false),
+                    new("Available roles", string.Join(", ", guildInfo.GuildRoles.Select(r => $"<@&{r.ID}>")))
                 },
                 Timestamp = DateTimeOffset.UtcNow,
-                Colour = _colourPalette.Blue,
+                Colour = _colourPalette.Blue
             };
 
-            if (guildInfo.Icon is not null)
+            if (guildInfo.GuildIconUrl is not null)
             {
-                var getIconUrlResult = CDN.GetGuildIconUrl(_context.GuildID.Value, guildInfo.Icon, CDNImageFormat.PNG);
-                
-                if (getIconUrlResult.IsSuccess)
+                embed = embed with
                 {
-                    embed = embed with
-                    {
-                        Thumbnail = new EmbedThumbnail(getIconUrlResult.Entity.ToString())
-                    };
-                }
+                    Thumbnail = new EmbedThumbnail(guildInfo.GuildIconUrl.ToString())
+                };
             }
 
             var sendMessageResult = await _channelApi.CreateMessageAsync(_context.ChannelID, embed: embed);
