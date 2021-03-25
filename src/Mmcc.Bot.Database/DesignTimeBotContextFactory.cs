270bb7a514a1d42759272e892e2757f219a1493e
Trace <lukaszdabrowski881@gmail.com>
2021-03-25 02:06:02 +0000

    reformat code

--- src/Mmcc.Bot.Database/DesignTimeBotContextFactory.cs
+++ src/Mmcc.Bot.Database/DesignTimeBotContextFactory.cs
@@ -16,18 +16,17 @@ namespace Mmcc.Bot.Database
                 .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../Mmcc.Bot"))
                 .AddJsonFile("appsettings.Development.json")
                 .Build();
-            
+
             // get connection string;
             var optionsBuilder = new DbContextOptionsBuilder<BotContext>();
             var boundConfig = config.GetSection("MySql").Get<MySqlSettings>();
             var connString =
                 $"Server={boundConfig.ServerIp};Port={boundConfig.Port};Database={boundConfig.DatabaseName};Uid={boundConfig.Username};Pwd={boundConfig.Password};Allow User Variables=True";
             optionsBuilder.UseMySql(
-                    connString,
-                    ServerVersion.FromString("10.4.11-mariadb"),
-                    b => b.MigrationsAssembly("Mmcc.Bot.Database")
-                    );
-            
+                connString,
+                ServerVersion.FromString("10.4.11-mariadb"),
+                b => b.MigrationsAssembly("Mmcc.Bot.Database"));
+
             return new BotContext(optionsBuilder.Options);
         }
     }
