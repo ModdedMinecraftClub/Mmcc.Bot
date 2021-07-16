0cb8bcb379104af82a554290133db6c633fad0e6
TraceLD <lukaszdabrowski881@gmail.com>
2021-07-16 02:34:00 +0200

    refactor timestamp styles into an enum

---
+++ src/Mmcc.Bot.RemoraAbstractions/Timestamps/DiscordTimestamp.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Mmcc.Bot.RemoraAbstractions.Timestamps
+{
+    // TODO: use this everywhere instead of DateTimeOffset.ToString();
+    /// <summary>
+    /// Represents a Discord timestamp.
+    /// </summary>
+    public readonly struct DiscordTimestamp : IEquatable<DiscordTimestamp>
+    {
+        /// <summary>
+        /// Creates a new <see cref="DiscordTimestamp"/> with the given <paramref name="value"/>.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        public DiscordTimestamp(DateTimeOffset value) =>
+            Value = value;
+
+        /// <summary>
+        /// The value.
+        /// </summary>
+        public DateTimeOffset Value { get; }
+
+        /// <summary>
+        /// Gets the unstyled Discord timestamp <see cref="string"/>.
+        /// </summary>
+        /// <returns>The unstyled Discord timestamp <see cref="string"/>.</returns>
+        public string AsUnstyled() =>
+            $"<t:{Value.ToUnixTimeSeconds()}>";
+
+        /// <summary>
+        /// Gets the styled Discord timestamp <see cref="string"/>.
+        /// </summary>
+        /// <param name="style">The style.</param>
+        /// <returns>The styled Discord timestamp <see cref="string"/>.</returns>
+        public string AsStyled(DiscordTimestampStyle style) =>
+            $"<t:{Value.ToUnixTimeSeconds()}:{(char)style}>";
+
+        /// <inheritdoc />
+        public override string ToString() =>
+            // this is the default style according to Discord API docs;
+            AsStyled(DiscordTimestampStyle.ShortDateTime);
+
+        /// <inheritdoc />
+        public override bool Equals(object? obj) =>
+            obj switch
+            {
+                DiscordTimestamp other => Equals(other),
+                _ => false
+            };
+
+        /// <inheritdoc />
+        public bool Equals(DiscordTimestamp other) =>
+            MemoryExtensions.Equals(Value, other.Value);
+
+        /// <inheritdoc />
+        public override int GetHashCode() =>
+            Value.GetHashCode();
+        
+        public static bool operator ==(DiscordTimestamp left, DiscordTimestamp right)
+            => left.Equals(right);
+        
+        public static bool operator !=(DiscordTimestamp left, DiscordTimestamp right)
+            => !left.Equals(right);
+    }
+}
---
+++ src/Mmcc.Bot.RemoraAbstractions/Timestamps/DiscordTimestampStyle.cs
@@ -0,0 +1,37 @@
+﻿namespace Mmcc.Bot.RemoraAbstractions.Timestamps
+{
+    /// <summary>
+    /// Enumerates the <see cref="DiscordTimestamp"/> styles.
+    /// </summary>
+    public enum DiscordTimestampStyle
+    {
+        /// <summary>
+        /// Represents Discord's "Short Time" (e.g. 16:20).
+        /// </summary>
+        ShortTime = 't',
+        /// <summary>
+        /// Represents Discord's "Long Time" (e.g. 16:20:30).
+        /// </summary>
+        LongTime = 'T',
+        /// <summary>
+        /// Represents Discord's "Short Date" (e.g. 20/04/2021).
+        /// </summary>
+        ShortDate = 'd',
+        /// <summary>
+        /// Represents Discord's "Long Date" (e.g. 20 April 2021).
+        /// </summary>
+        LongDate = 'D',
+        /// <summary>
+        /// Represents Discord's "Short Date/Time" (e.g. 20 April 2021 16:20).
+        /// </summary>
+        ShortDateTime = 'f',
+        /// <summary>
+        /// Represents Discord's "Long Date/Time" (e.g. Tuesday, 20 April 2021 16:20).
+        /// </summary>
+        LongDateTime = 'F',
+        /// <summary>
+        /// Represents Discord's "Relative Time" (e.g. 2 months ago).
+        /// </summary>
+        RelativeTime = 'R'
+    }
+}
