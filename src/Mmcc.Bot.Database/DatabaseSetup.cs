c1b1d8106dba78494a2060d1121945bd97f8fff6
TraceLD <lukaszdabrowski881@gmail.com>
2021-07-27 05:41:06 +0100

    clean up Program.cs

---
+++ src/Mmcc.Bot.Database/DatabaseSetup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Mmcc.Bot.Database.Settings;
+
+namespace Mmcc.Bot.Database
+{
+    /// <summary>
+    /// Extension methods that configure and register the DB context with the service provider.
+    /// </summary>
+    public static class DatabaseSetup
+    {
+        /// <summary>
+        /// Configures and registers the DB context with the service provider.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+        /// <returns>The <see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection AddBotDatabaseContext(this IServiceCollection services) =>
+            services.AddDbContext<BotContext>((provider, options) =>
+            {
+                var dbConfig = provider.GetRequiredService<MySqlSettings>();
+                var connString = dbConfig.ConnectionString;
+                var serverVersion = ServerVersion.Parse(dbConfig.MySqlVersionString);
+                var retryAmount = dbConfig.RetryAmount;
+
+                options.UseMySql(connString, serverVersion,
+                    contextOptions => contextOptions.EnableRetryOnFailure(retryAmount));
+            });
+    }
+}
---
+++ src/Mmcc.Bot/Hosting/HostingSetup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Mmcc.Bot.Hosting.Moderation;
+
+namespace Mmcc.Bot.Hosting
+{
+    /// <summary>
+    /// Extension methods that register bot hosted services with the service collection.
+    /// </summary>
+    public static class MiddlewareSetup
+    {
+        /// <summary>
+        /// Registers bot hosted services with the service collection.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+        /// <returns>The <see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection AddBotBackgroundServices(this IServiceCollection services) =>
+            services.AddHostedService<ModerationBackgroundService>();
+    }
+}
