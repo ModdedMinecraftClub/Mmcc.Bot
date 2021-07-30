627b7abd1f9cad84f38492a43d52789bba1a40a8
TraceLD <lukaszdabrowski881@gmail.com>
2021-07-30 13:55:15 +0100

    add application parser

---
+++ src/Mmcc.Bot.Common/Models/DiscordMemberApplication.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Mmcc.Bot.Common.Models
+{
+    /// <summary>
+    /// Represents a parsed Discord MMCC member application.
+    /// </summary>
+    /// <param name="IGNs">IGNs of the people applying</param>
+    /// <param name="Server"></param>
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public record DiscordMemberApplication(
+        string Server,
+        List<string> IGNs
+    );
+}
