71308d2a452ae826c06ec5ae6563e0e25459264f
TraceLD <lukaszdabrowski881@gmail.com>
2021-10-12 00:05:51 +0100

    update Program.cs to use top level statements

--- src/Mmcc.Bot/Program.cs
+++ src/Mmcc.Bot/Program.cs
@@ -5,6 +5,7 @@ using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Mmcc.Bot;
 using Mmcc.Bot.Behaviours;
 using Mmcc.Bot.Caching;
 using Mmcc.Bot.Commands;
@@ -15,6 +16,7 @@ using Mmcc.Bot.Database.Settings;
 using Mmcc.Bot.EventResponders;
 using Mmcc.Bot.EventResponders.Moderation.MemberApplications;
 using Mmcc.Bot.Hosting;
+using Mmcc.Bot.Hosting.Moderation;
 using Mmcc.Bot.Middleware;
 using Mmcc.Bot.Mojang;
 using Mmcc.Bot.Polychat;
@@ -27,97 +29,85 @@ using Serilog.Events;
 using Serilog.Formatting.Compact;
 using Serilog.Sinks.SystemConsole.Themes;
 
-namespace Mmcc.Bot
-{
-    public class Program
+var isDevelopment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT")?.Equals("Development") ?? false;
+var hostBuilder = Host.CreateDefaultBuilder(args)
+    .ConfigureLogging((context, builder) =>
     {
-        public static void Main(string[] args)
+        if (context.HostingEnvironment.IsDevelopment())
         {
-            var isDevelopment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT")?.Equals("Development") ??
-                                false;
-
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Information()
-                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-                .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
-                .MinimumLevel.Override("System.Net.Http.HttpClient",
-                    isDevelopment ? LogEventLevel.Information : LogEventLevel.Warning)
-                .Enrich.FromLogContext()
-                .WriteTo.Console(
-                    outputTemplate: "[{Timestamp:dd/MM/yyyy HH:mm:ss:fff} {Level:u3}] {Message:lj}{NewLine}{Exception}",
-                    theme: isDevelopment ? null : AnsiConsoleTheme.Literate
-                )
-                .WriteTo.File(
-                    new CompactJsonFormatter(),
-                    Path.Combine("logs", "log.clef"),
-                    rollingInterval: RollingInterval.Day,
-                    retainedFileCountLimit: 14,
-                    levelSwitch: new LoggingLevelSwitch(LogEventLevel.Warning)
-                )
-                .CreateLogger();
-
-            try
-            {
-                Log.Information("Starting the host...");
-                CreateHostBuilder(args).Build().Run();
-            }
-            catch (Exception e)
-            {
-                Log.Fatal(e, "Host terminated unexpectedly.");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
+            builder.AddDebug();
         }
+    })
+    .ConfigureServices((hostContext, services) =>
+    {
+        // config;
+        services.ConfigureBot(hostContext);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureLogging((context, builder) =>
-                {
-                    if (context.HostingEnvironment.IsDevelopment())
-                    {
-                        builder.AddDebug();
-                    }
-                })
-                .ConfigureServices((hostContext, services) =>
-                {
-                    // config;
-                    services.ConfigureBot(hostContext);
-                    
-                    // db;
-                    services.AddBotDatabaseContext();
-                    
-                    services.AddSingleton<IColourPalette, TailwindColourPalette>();
-                    
-                    // FluentValidation;
-                    services.AddValidatorsFromAssemblyContaining<Program>();
-                    services.AddValidatorsFromAssemblyContaining<DiscordSettings>();
-                    services.AddValidatorsFromAssemblyContaining<MySqlSettingsValidator>();
-                    
-                    // MediatR;
-                    services.AddMediatR(typeof(CreateFromDiscordMessage), typeof(TcpRequest<>));
-                    services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
-                    
-                    // Mmcc.Bot.X projects;
-                    services.AddMmccCaching();
-                    services.AddMojangApi();
-                    services.AddPolychat(hostContext.Configuration.GetSection("Ssmp"));
-                    
-                    // Remora.Discord bot setup;
-                    services.AddRemoraAbstractions();
-                    services.AddBotMiddlewares();
-                    services.AddBotCommands();
-                    services.AddBotGatewayEventResponders();
-                    services.AddDiscordCaching();
-                    services.AddBotBackgroundServices();
-                })
-                .AddDiscordService(provider =>
-                {
-                    var discordConfig = provider.GetRequiredService<DiscordSettings>();
-                    return discordConfig.Token;
-                })
-                .UseDefaultServiceProvider(options => options.ValidateScopes = true)
-                .UseSerilog();
-    }
+        // db;
+        services.AddBotDatabaseContext();
+
+        services.AddSingleton<IColourPalette, TailwindColourPalette>();
+
+        // FluentValidation;
+        services.AddValidatorsFromAssemblyContaining<GetExpiredActions>();
+        services.AddValidatorsFromAssemblyContaining<DiscordSettings>();
+        services.AddValidatorsFromAssemblyContaining<MySqlSettingsValidator>();
+
+        // MediatR;
+        services.AddMediatR(typeof(CreateFromDiscordMessage), typeof(TcpRequest<>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+        // Mmcc.Bot.X projects;
+        services.AddMmccCaching();
+        services.AddMojangApi();
+        services.AddPolychat(hostContext.Configuration.GetSection("Ssmp"));
+
+        // Remora.Discord bot setup;
+        services.AddRemoraAbstractions();
+        services.AddBotMiddlewares();
+        services.AddBotCommands();
+        services.AddBotGatewayEventResponders();
+        services.AddDiscordCaching();
+        services.AddBotBackgroundServices();
+    })
+    .AddDiscordService(provider =>
+    {
+        var discordConfig = provider.GetRequiredService<DiscordSettings>();
+        return discordConfig.Token;
+    })
+    .UseDefaultServiceProvider(options => options.ValidateScopes = true)
+    .UseSerilog();
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+    .MinimumLevel.Override("System.Net.Http.HttpClient",
+        isDevelopment ? LogEventLevel.Information : LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Console(
+        outputTemplate: "[{Timestamp:dd/MM/yyyy HH:mm:ss:fff} {Level:u3}] {Message:lj}{NewLine}{Exception}",
+        theme: isDevelopment ? null : AnsiConsoleTheme.Literate
+    )
+    .WriteTo.File(
+        new CompactJsonFormatter(),
+        Path.Combine("logs", "log.clef"),
+        rollingInterval: RollingInterval.Day,
+        retainedFileCountLimit: 14,
+        levelSwitch: new LoggingLevelSwitch(LogEventLevel.Warning)
+    )
+    .CreateLogger();
+
+try
+{
+    Log.Information("Starting the host...");
+    hostBuilder.Build().Run();
+}
+catch (Exception e)
+{
+    Log.Fatal(e, "Host terminated unexpectedly.");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
