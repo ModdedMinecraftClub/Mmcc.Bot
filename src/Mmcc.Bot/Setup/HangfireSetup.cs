c2c119c0a2a4d498d4b77874aa8531d237a99aa1
TraceLD <lukaszdabrowski881@gmail.com>
2022-05-13 21:16:46 +0200

    log Hangfire startup exceptions

--- src/Mmcc.Bot/Setup/HangfireSetup.cs
+++ src/Mmcc.Bot/Setup/HangfireSetup.cs
@@ -4,6 +4,7 @@ using Hangfire.Storage.MySql;
 using Microsoft.Extensions.DependencyInjection;
 using Mmcc.Bot.Database.Settings;
 using Mmcc.Bot.Polychat.Jobs;
+using Microsoft.Extensions.Logging;
 
 namespace Mmcc.Bot.Setup;
 
@@ -11,22 +12,34 @@ public static class HangfireSetup
 {
     public static IServiceCollection AddHangfire(this IServiceCollection services)
     {
-        services.AddHangfire((provider, configuration) => configuration
-            .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
-            .UseSimpleAssemblyNameTypeSerializer()
-            .UseRecommendedSerializerSettings()
-            .UseStorage(
-                new MySqlStorage(provider.GetRequiredService<MySqlSettings>().ConnectionString,
-                    new MySqlStorageOptions
-                    {
-                        QueuePollInterval = TimeSpan.FromSeconds(15),
-                        JobExpirationCheckInterval = TimeSpan.FromMinutes(20),
-                        CountersAggregateInterval = TimeSpan.FromMinutes(5),
-                        PrepareSchemaIfNecessary = true,
-                        TransactionTimeout = TimeSpan.FromMinutes(1),
-                        TablesPrefix = "__Hangfire"
-                    })
-            ));
+        services.AddHangfire((provider, configuration) =>
+        {
+            var logger = provider.GetRequiredService<ILogger<Program>>();
+            
+            try
+            {
+                configuration
+                    .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                    .UseSimpleAssemblyNameTypeSerializer()
+                    .UseRecommendedSerializerSettings()
+                    .UseStorage(
+                        new MySqlStorage(provider.GetRequiredService<MySqlSettings>().ConnectionString,
+                            new MySqlStorageOptions
+                            {
+                                QueuePollInterval = TimeSpan.FromSeconds(15),
+                                JobExpirationCheckInterval = TimeSpan.FromMinutes(20),
+                                CountersAggregateInterval = TimeSpan.FromMinutes(5),
+                                PrepareSchemaIfNecessary = true,
+                                TransactionTimeout = TimeSpan.FromMinutes(1),
+                                TablesPrefix = "__Hangfire"
+                            })
+                    );
+            }
+            catch(Exception e)
+            {
+                logger.LogError(e, "Error while setting up Hangfire");
+            }
+        });
         
         services.AddHangfireServer(o =>
         {
