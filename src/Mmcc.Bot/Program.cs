e75054d1ebeafec8a2a197fd2db992bcef16fd1d
TraceLD <lukaszdabrowski881@gmail.com>
2022-06-25 20:06:10 +0100

    remove unused usings

--- src/Mmcc.Bot/Program.cs
+++ src/Mmcc.Bot/Program.cs
@@ -1,7 +1,6 @@
 using System;
 using FluentValidation;
 using MediatR;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
